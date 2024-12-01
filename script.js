ac1168b7fbd281c8b4523d6e23b502d23626cf3e
ismailtrm <ismailtarim7@gmail.com>
2024-12-02 02:43:41 +0300

    fix: images and general editing

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let slideIndex = 0;
 const slidesToShow = 3;
-const slideWidth = 100 / slidesToShow; // Calculate the width of each slide as a percentage
+const slideWidth = 100 / slidesToShow;
 
 showSlides(slideIndex);
 
 function showSlides(n) {
-    const slider = document.querySelector('.slider-slider__swiper');
-    const totalSlides = document.querySelectorAll('.slider-slider__slide').length;
+    const slider = document.querySelector('.slider');
+    const totalSlides = document.querySelectorAll('.slide').length;
 
     if (n >= totalSlides - slidesToShow + 1) { slideIndex = 0; }
     if (n < 0) { slideIndex = totalSlides - slidesToShow; }
@@ -14,15 +14,14 @@ function showSlides(n) {
     const offset = -slideIndex * slideWidth;
     slider.style.transform = `translateX(${offset}%)`;
 
-    // Enable/disable navigation buttons
-    document.querySelector('.slider-slider__nav-button_prev').disabled = slideIndex === 0;
-    document.querySelector('.slider-slider__nav-button_next').disabled = slideIndex >= totalSlides - slidesToShow;
+    document.querySelector('.slider-nav-button_prev').disabled = slideIndex === 0;
+    document.querySelector('.slider-nav-button_next').disabled = slideIndex >= totalSlides - slidesToShow;
 }
 
-document.querySelector('.slider-slider__nav-button_prev').addEventListener('click', () => {
+document.querySelector('.slider-nav-button_prev').addEventListener('click', () => {
     showSlides(--slideIndex);
 });
 
-document.querySelector('.slider-slider__nav-button_next').addEventListener('click', () => {
+document.querySelector('.slider-nav-button_next').addEventListener('click', () => {
     showSlides(++slideIndex);
 });
